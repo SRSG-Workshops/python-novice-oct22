5ba4cec70fbe167042b8362fce185ed96cf7ab9e
Ben Anderson <dataknut@icloud.com>
2022-06-15 12:39:27 +0100

    fix to file paths if running code from repo base (instead of inside the code folder).

--- code/climate_analysis-1.py
+++ code/climate_analysis-1.py
@@ -1,4 +1,7 @@
-climate_data = open('../data/sc_climate_data_10.csv', 'r')
+#climate_data = open('../data/sc_climate_data_10.csv', 'r')
+
+# if running from the repo base:
+climate_data = open('data/sc_climate_data_10.csv', 'r')
 
 for line in climate_data:
     print(line)
